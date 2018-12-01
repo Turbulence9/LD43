3451f838c85e44f7ec7c7520a43f95dd58c85265
Jacob Greenway <jagreenway12@gmail.com>
2018-12-01 00:42:11 -0600

    tile rendering

--- js/app.js
+++ js/app.js
@@ -1,22 +1,52 @@
 let canvas = document.getElementById("canvas");
 ctx = canvas.getContext("2d");
-let aspectRatio = 16/10;
+let aspectRatio = 16 / 10;
 let windowedWidth = 1024;
 let windowedHeight = 640;
 canvas.width = windowedWidth;
 canvas.height = windowedHeight;
 
+// Generic tile properties
+let tileSize = 16;
+
+// Initialize generic objects (feel free to use any name here that's convient)
+let floor = {
+  // TODO: Add sprite here later (will render red rect if not specified)
+  sprite: null
+};
+
+// Update current level when we make it to the next level (set initially to first level)
+let currentLevel = exampleLevel;
+
 function update() {
-  console.log("in update");
+  // Loop through current level tiles and render objects to grid
+  // Dynamically changes size of grids depending on number of grids in level to fill the canvas
+  for (let i = 0; i < currentLevel.length; i++) {
+    for (let j = 0; j < currentLevel[i].length; j++) {
+      // Get the sprite for the object
+      let tile = currentLevel[i][j];
+      if (tile && tile != 0) {
+        if (tile.sprite) {
+          // TODO: Draw the sprite on the canvas (use object properties to draw the sprite with correct width/height)
+          ctx.drawImage(tile.sprite);
+        } else {
+          // Draw a red sqaure if no sprite for debug puposes
+          ctx.fillStyle = "#FF0000";
+          ctx.fillRect(j * tileSize, i * tileSize, tileSize, tileSize);
+        }
+      }
+    }
+  }
+
   requestAnimationFrame(update);
 }
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   update();
 });
 
-document.addEventListener("keydown", function(e) {
+document.addEventListener("keydown", function (e) {
 }, false);
 
-document.addEventListener("keyup", function(e) {
+document.addEventListener("keyup", function (e) {
 }, false);
---
+++ js/levels.js
@@ -0,0 +1,8 @@
+// Levels
+// Example Level (3x3 grid for simplicity of example)
+const exampleLevel = [
+  [floor, floor, floor],
+  [floor, null, floor],
+  [null, floor, floor],
+  [floor, floor]
+];
