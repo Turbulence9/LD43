6aecd3a028ea1d40f649af0438b84dfc422cb78c
Alan-S-001 <alan.greenknight@gmail.com>
2018-12-01 12:31:15 -0600

    Create lever class for lever state management, similar to pressure plate

---
+++ js/lever.js
@@ -0,0 +1,33 @@
+class Lever {
+  constructor() {
+    this.state = {
+      x: null,
+      y: null,
+      width: 16,
+      height: 16,
+      active: false
+    };
+  }
+
+  create(xValue, yValue) {
+    this.state.x = xValue;
+    this.state.y = yValue;
+  }
+
+  setDimensions(width, height) {
+    this.state.width = width;
+    this.state.height = height;
+  }
+
+  activate() {
+    this.state.active = true;
+  }
+
+  deactivate() {
+    this.state.active = false;
+  }
+
+  isActive() {
+    return this.state.active;
+  }
+}
